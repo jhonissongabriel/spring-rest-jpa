7fc77ada949569671dafaeaccd564008ec5f4b03
Jhonisson Gabriel <jhonisson.gabriel@gmail.com>
2018-09-30 18:22:18 -0300

    Primeiro Commit - Criação do projeto spring data rest

---
+++ src/main/java/br/com/lab5/SpringDataRestProjetoFaculdadeApplication.java
@@ -0,0 +1,12 @@
+package br.com.lab5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataRestProjetoFaculdadeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataRestProjetoFaculdadeApplication.class, args);
+	}
+}
